191e1417b70fee3ea88d65b5571d69a736b5fa4b
Gustavo <prestesgustavo999@gmail.com>
2023-09-22 10:59:36 -0400

    modified:   lib/paginas/cadastro.dart 	modified:   lib/paginas/login.dart

--- lib/paginas/cadastro.dart
+++ lib/paginas/cadastro.dart
@@ -21,149 +21,151 @@ class _CadastroState extends State<Cadastro> {
                 gradient: LinearGradient(
                     colors: [Cores.pretogradiente, Cores.azulgradiente])),
           ),
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 30.0),
-                child: Center(
-                  child: Image.asset(
-                    "assets/images/logo.png",
-                    width: 150,
-                    height: 150,
+          SingleChildScrollView( // Added SingleChildScrollView here
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 30.0),
+                  child: Center(
+                    child: Image.asset(
+                      "assets/images/logo.png",
+                      width: 150,
+                      height: 150,
+                    ),
                   ),
                 ),
-              ),
-              const Text(
-                "CADASTRO",
-                style: TextStyle(color: Colors.white),
-              ),
-              const SizedBox(height: 60),
-              Container(
-                width: 339,
-                height: 350,
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.blue.shade900),
-                    borderRadius: BorderRadius.circular(20)),
-                child: Column(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Nome",
-                          style: TextStyle(color: Colors.white, fontSize: 15),
-                        ),
-                      ),
-                    ),
-                    TextField(
-                      autofocus: true,
-                      textAlign: TextAlign.left,
-                      keyboardType: TextInputType.name,
-                      decoration: InputDecoration(
-                          labelStyle: const TextStyle(color: Colors.white),
-                          contentPadding: const EdgeInsets.symmetric(vertical: 10),
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30))),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.all(8),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "E-mail",
-                          style: TextStyle(color: Colors.white, fontSize: 15),
-                        ),
-                      ),
-                    ),
-                    TextField(
-                      autofocus: true,
-                      textAlign: TextAlign.left,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                          labelStyle: const TextStyle(color: Colors.white),
-                          contentPadding: const EdgeInsets.symmetric(vertical: 10),
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(60))),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.all(8),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Senha",
-                          style: TextStyle(color: Colors.white, fontSize: 15),
-                        ),
-                      ),
-                    ),
-                    TextField(
-                      autofocus: true,
-                      obscureText: true,
-                      textAlign: TextAlign.left,
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                          labelStyle: const TextStyle(color: Colors.white),
-                          contentPadding: const EdgeInsets.symmetric(vertical: 10),
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(60))),
-                    ),
-                    Padding(
-                        padding: const EdgeInsets.only(top: 16.0),
-                        child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: ((context) => const TelaInicial())));
-                            },
-                            style: ElevatedButton.styleFrom(
-                                padding: const EdgeInsets.all(8),
-                                backgroundColor:
-                                    const Color.fromARGB(255, 22, 42, 196)),
-                            child: const Text(
-                              "Cadastrar",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                            ))),
-                  ],
+                const Text(
+                  "CADASTRO",
+                  style: TextStyle(color: Colors.white),
                 ),
-              ),
-              const SizedBox(
-                height: 60,
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "Possui login?",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+                const SizedBox(height: 60),
+                Container(
+                  width: 339,
+                  height: 350,
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blue.shade900),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Nome",
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                      ),
+                      TextField(
+                        autofocus: true,
+                        textAlign: TextAlign.left,
+                        keyboardType: TextInputType.name,
+                        decoration: InputDecoration(
+                            labelStyle: const TextStyle(color: Colors.white),
+                            contentPadding: const EdgeInsets.symmetric(vertical: 10),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30))),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "E-mail",
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                      ),
+                      TextField(
+                        autofocus: true,
+                        textAlign: TextAlign.left,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            labelStyle: const TextStyle(color: Colors.white),
+                            contentPadding: const EdgeInsets.symmetric(vertical: 10),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(60))),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Senha",
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                      ),
+                      TextField(
+                        autofocus: true,
+                        obscureText: true,
+                        textAlign: TextAlign.left,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            labelStyle: const TextStyle(color: Colors.white),
+                            contentPadding: const EdgeInsets.symmetric(vertical: 10),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(60))),
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.only(top: 16.0),
+                          child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: ((context) => const TelaInicial())));
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  padding: const EdgeInsets.all(8),
+                                  backgroundColor:
+                                      const Color.fromARGB(255, 22, 42, 196)),
+                              child: const Text(
+                                "Cadastrar",
+                                style:
+                                    TextStyle(color: Colors.white, fontSize: 20),
+                              ))),
+                    ],
                   ),
                 ),
-              ),
-              SizedBox(
-                  width: 100,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 16.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => const Login()));
-                      },
-                      style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.all(8),
-                          backgroundColor: const Color.fromARGB(255, 22, 42, 196)),
-                      child: const Text(
-                        "Entrar",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
+                const SizedBox(
+                  height: 60,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Possui login?",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
                     ),
-                  ))
-            ],
+                  ),
+                ),
+                SizedBox(
+                    width: 100,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => const Login()));
+                        },
+                        style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.all(8),
+                            backgroundColor: const Color.fromARGB(255, 22, 42, 196)),
+                        child: const Text(
+                          "Entrar",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                    ))
+              ],
+            ),
           )
         ],
       ),
--- lib/paginas/login.dart
+++ lib/paginas/login.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:studyroutine/_cor/paleta.dart';
 import 'package:studyroutine/paginas/cadastro.dart';
 import 'package:studyroutine/paginas/inicio.dart';
-import 'package:studyroutine/paginas/tela_boas_vindas.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -22,141 +21,143 @@ class _LoginState extends State<Login> {
                 gradient: LinearGradient(
                     colors: [Cores.pretogradiente, Cores.azulgradiente])),
           ),
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 30.0),
-                child: Center(
-                  child: Image.asset(
-                    'assets/images/logo.png',
-                    width: 150,
-                    height: 150,
+          SingleChildScrollView( // Added SingleChildScrollView here
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 30.0),
+                  child: Center(
+                    child: Image.asset(
+                      'assets/images/logo.png',
+                      width: 150,
+                      height: 150,
+                    ),
                   ),
                 ),
-              ),
-              const Text(
-                'LOGIN',
-                style: TextStyle(color: Colors.white),
-              ),
-              const SizedBox(
-                height: 60,
-              ),
-              Container(
-                width: 339,
-                height: 360,
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Cores.azulgradiente,
-                  ),
-                  borderRadius: BorderRadius.circular(20),
+                const Text(
+                  'LOGIN',
+                  style: TextStyle(color: Colors.white),
                 ),
-                child: Column(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.all(8),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'E-mail',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
+                const SizedBox(
+                  height: 60,
+                ),
+                Container(
+                  width: 339,
+                  height: 360,
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Cores.azulgradiente,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'E-mail',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    TextField(
-                      textAlign: TextAlign.left,
-                      decoration: InputDecoration(
-                          labelStyle: const TextStyle(color: Colors.white),
-                          contentPadding:
-                              const EdgeInsets.symmetric(vertical: 10),
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(60)),
-                          filled: true,
-                          fillColor: Colors.white),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.all(8),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Senha',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(color: Colors.white, fontSize: 20),
+                      TextField(
+                        textAlign: TextAlign.left,
+                        decoration: InputDecoration(
+                            labelStyle: const TextStyle(color: Colors.white),
+                            contentPadding:
+                                const EdgeInsets.symmetric(vertical: 10),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(60)),
+                            filled: true,
+                            fillColor: Colors.white),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Senha',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
                         ),
                       ),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          contentPadding:
-                              const EdgeInsets.symmetric(vertical: 10),
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(90)),
-                          filled: true,
-                          fillColor: Colors.white),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Row(
-                        children: [
-                          const Text(
-                            'Continue com o Google',
-                            style: TextStyle(color: Colors.white, fontSize: 12),
-                          ),
-                          Image.asset(
-                            'assets/images/google.png',
-                            width: 20,
-                            height: 20,
-                          )
-                        ],
+                      TextField(
+                        decoration: InputDecoration(
+                            contentPadding:
+                                const EdgeInsets.symmetric(vertical: 10),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(90)),
+                            filled: true,
+                            fillColor: Colors.white),
                       ),
-                    ),
-                    SizedBox(
-                        height: 30,
-                        child: ElevatedButton(
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Row(
+                          children: [
+                            const Text(
+                              'Continue com o Google',
+                              style: TextStyle(color: Colors.white, fontSize: 12),
+                            ),
+                            Image.asset(
+                              'assets/images/google.png',
+                              width: 20,
+                              height: 20,
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                          height: 30,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    const Color.fromARGB(255, 22, 42, 196)),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const Inicio()));
+                            },
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          )),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        'Não tem uma conta? Faça seu cadastro ',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      ElevatedButton(
                           style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  const Color.fromARGB(255, 22, 42, 196)),
+                              backgroundColor: const Color.fromARGB(255, 22, 42, 196)),
                           onPressed: () {
                             Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => const Inicio()));
+                                    builder: ((context) => const Cadastro())));
                           },
                           child: const Text(
-                            'Login',
+                            'Cadastrar',
                             style: TextStyle(color: Colors.white),
-                          ),
-                        )),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const Text(
-                      'Não tem uma conta? Faça seu cadastro ',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color.fromARGB(255, 22, 42, 196)),
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: ((context) => const Cadastro())));
-                        },
-                        child: const Text(
-                          'Cadastrar',
-                          style: TextStyle(color: Colors.white),
-                        ))
-                  ],
-                ),
-              )
-            ],
+                          ))
+                    ],
+                  ),
+                )
+              ],
+            ),
           )
         ],
       ),
